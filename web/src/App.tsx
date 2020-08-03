c8bfd02d209d05c1ae1d1ace60d9c2679f7aa47a
Dtesch9 <alucard.s8123@gmail.com>
2020-08-03 14:55:14 -0300

    chore: Landing Page Layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
 import './assets/styles/global.css';
 
-const App: React.FC = () => {
-  return <h1>NLW :)</h1>;
-};
+const App: React.FC = () => <Landing />;
 
 export default App;
