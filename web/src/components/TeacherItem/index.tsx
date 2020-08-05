6db3e5e4700a95061451ebacd7993fb0587e329d
Dtesch9 <alucard.s8123@gmail.com>
2020-08-05 14:24:24 -0300

    feat: Search for class

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  subject: string;
+  whatsapp: string;
+}
+
+type TeacherItemProps = {
+  teacher: Teacher;
+};
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const createNewConnection = useCallback(() => {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }, [teacher]);
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://api.adorable.io/avatars/55/abott@adorable.png"
-          alt="Douglas Tesch"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Douglas Tesch</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores químicas do Brasil brasileiro ou até mesmo do
-        mundo
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback, FormEvent } from 'react';
+
+import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import { Input, Select } from '../../components/Form';
 
 import './styles.css';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState<Teacher[]>([]);
+
+  const [subject, setSubject] = useState('');
+  const [weekDay, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const searchTeachers = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+
+      const response = await api.get('/classes', {
+        params: {
+          subject,
+          week_day: weekDay,
+          time,
+        },
+      });
+
+      setTeachers(response.data);
+    },
+    [subject, weekDay, time],
+  );
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
           <Select
             name="subject"
             label="Matéria"
+            value={subject}
+            onChange={event => {
+              setSubject(event.target.value);
+            }}
             options={[
               { value: 'Artes', label: 'Artes' },
               { value: 'Física', label: 'Física' },
@@ -33,6 +62,10 @@ const TeacherList: React.FC = () => {
           <Select
             name="weel_day"
             label="Dia da semana"
+            value={weekDay}
+            onChange={event => {
+              setWeekDay(event.target.value);
+            }}
             options={[
               { value: '0', label: 'Domingo' },
               { value: '1', label: 'Segunda-feira' },
@@ -43,17 +76,24 @@ const TeacherList: React.FC = () => {
               { value: '6', label: 'Sábado' },
             ]}
           />
-          <Input type="time" name="time" label="Hora" />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={event => {
+              setTime(event.target.value);
+            }}
+          />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map(teacher => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   );
