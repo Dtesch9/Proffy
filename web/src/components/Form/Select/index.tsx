5dfae501408605e6001ff499397ddcf510728bfd
Dtesch9 <alucard.s8123@gmail.com>
2020-08-05 13:51:30 -0300

    feat: Teacher create class

--- web/src/components/Form/Select/index.tsx
+++ web/src/components/Form/Select/index.tsx
@@ -20,7 +20,7 @@ const Select: React.FC<SelectProps> = props => {
     <div className="select-block">
       <label htmlFor={name}>
         {label}
-        <select defaultValue="" id={name} {...rest}>
+        <select value="" id={name} {...rest}>
           <option value="" disabled hidden>
             Selecione uma opção
           </option>
