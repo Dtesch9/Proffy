d06cbc713708d00484b29b437d4e87b3d9c7a8c2
Dtesch9 <alucard.s8123@gmail.com>
2020-08-07 15:15:58 -0300

    feat: List favorite teachers

--- mobile/src/pages/Study/Favorites/index.tsx
+++ mobile/src/pages/Study/Favorites/index.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import PageHeader from '../../../components/PageHeader';
-import TeacherItem from '../../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../../components/TeacherItem';
 
 import { Container, TeachersList } from './styles';
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState<Teacher[]>([]);
+
+  useFocusEffect(() => {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        setFavorites(JSON.parse(response));
+      }
+    });
+  });
+
   return (
     <Container>
       <PageHeader title="Meus proffys favoritos" />
 
       <TeachersList>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map(teacher => (
+          <TeacherItem key={teacher.id} teacher={teacher} isFavorite />
+        ))}
       </TeachersList>
     </Container>
   );
